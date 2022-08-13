c9dea4af272a0635c2301f08b6af6032759447d1
Andrew Miller <62162992+AndrewPatrickMiller@users.noreply.github.com>
2022-08-13 17:48:29 -0600

    made basic dot shrink

--- projects/basic-d3-app/src/app/app.module.ts
+++ projects/basic-d3-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DotDirective } from './directives/dot.directive';
+import { BasicDotComponent } from './basic-dot/basic-dot.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DotDirective
+    DotDirective,
+    BasicDotComponent
   ],
   imports: [
     BrowserModule
