eed160aa78da2b6ddc35ccec7877b87561583abe
Andrew Miller <62162992+AndrewPatrickMiller@users.noreply.github.com>
2022-08-13 17:32:10 -0600

    📅  | Sat Aug 13 17:32:10 MDT 2022

--- projects/basic-d3-app/src/app/directives/dot.directive.ts
+++ projects/basic-d3-app/src/app/directives/dot.directive.ts
@@ -1,27 +1,29 @@
-import * as d3 from "d3"
-import {Directive, ElementRef} from '@angular/core';
+import * as d3 from 'd3';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
-  selector: '[basicDot]'
+  selector: '[basicDot]',
 })
 export class DotDirective {
-  mockDataOne = [30, 2, 3]
+  mockDataOne = [30, 2, 3];
   selectedElement: d3.Selection<HTMLElement, any, any, any>;
   constructor(private eleRef: ElementRef) {
-    const containerEle: HTMLElement = this.eleRef.nativeElement
-    this.selectedElement = d3.select(containerEle)
-    this.createDot()
+    const containerEle: HTMLElement = this.eleRef.nativeElement;
+    this.selectedElement = d3.select(containerEle);
+    this.createDot();
   }
 
   createDot() {
     const setAttr: d3.ValueFn<any, any, any> = (data, index, element) => {
-      return data[0]
-    }
+      return data[0];
+    };
 
-    this.selectedElement.append('svg').append('circle').datum(this.mockDataOne)
+    this.selectedElement
+      .append('svg')
+      .append('circle')
+      .datum(this.mockDataOne)
       .attr('cy', setAttr)
       .attr('cx', setAttr)
-      .attr('r', setAttr)
+      .attr('r', setAttr);
   }
-
 }
