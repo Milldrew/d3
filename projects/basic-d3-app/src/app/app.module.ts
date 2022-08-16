12b1c8c6017bc0e38fdbecaa7416b39a08ff8b50
Andrew Miller <62162992+AndrewPatrickMiller@users.noreply.github.com>
2022-08-15 20:46:38 -0600

    📅  | Mon Aug 15 20:46:38 MDT 2022

--- projects/basic-d3-app/src/app/app.module.ts
+++ projects/basic-d3-app/src/app/app.module.ts
@@ -2,19 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { DotDirective } from './directives/dot.directive';
 import { BasicDotComponent } from './basic-dot/basic-dot.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DotDirective,
-    BasicDotComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, BasicDotComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- projects/basic-d3-app/src/app/directives/dot.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DotDirective } from './dot.directive';
-
-describe('DotDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DotDirective();
-    expect(directive).toBeTruthy();
-  });
-});
