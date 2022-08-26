ae864787db5479672d820cf914a6d8a160a96246
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-08-26 06:25:02 -0400

    📅  | Fri Aug 26 06:25:02 EDT 2022

--- projects/basic-d3-app/src/app/app.module.ts
+++ projects/basic-d3-app/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BasicDotComponent } from './basic-dot/basic-dot.component';
+import { DotSvgComponent } from './dot-svg/dot-svg.component';
 
 @NgModule({
-  declarations: [AppComponent, BasicDotComponent],
+  declarations: [AppComponent, BasicDotComponent, DotSvgComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ projects/basic-d3-app/src/app/dot-svg/dot-svg.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'basic-dot-svg',
+  templateUrl: './dot-svg.component.html',
+  styleUrls: ['./dot-svg.component.scss']
+})
+export class DotSvgComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
