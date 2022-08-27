cf80c1e60dcaf0de302280b403a0d30bf977f933
Andrew Miller <62162992+AndrewPatrickMiller@users.noreply.github.com>
2022-08-27 19:25:17 -0400

    📅  | Sat Aug 27 19:25:17 EDT 2022

--- projects/basic-d3-app/src/app/basic-svg/dot-svg.component.ts
+++ projects/basic-d3-app/src/app/basic-svg/dot-svg.component.ts
@@ -11,11 +11,17 @@ export class BasicSvgComponent {
   parentMostElement: d3.Selection<HTMLElement, any, any, any>;
   svgElement: d3.Selection<SVGSVGElement, any, any, any>;
 
+  count = 0;
   constructor(private eleRef: ElementRef) {
     const containerEle: HTMLElement = this.eleRef.nativeElement;
     this.parentMostElement = d3.select(containerEle);
     this.addSvgElement();
     this.addViewBoxToSvg();
+    this.appendViewViewBox();
+    this.appendViewViewBox();
+    this.appendViewViewBox();
+    this.appendViewViewBox();
+    this.appendViewViewBox();
   }
 
   addSvgElement() {
@@ -27,9 +33,10 @@ export class BasicSvgComponent {
     this.svgElement.attr('viewBox', () => '0 0 40 40');
   }
 
-  sizeAddColorToSvg() {}
-
-  addResponsiveSizing() {}
+  appendViewViewBox() {
+    this.count += 1;
+    this.svgElement.append('p').text(this.count);
+  }
 
   ngOnChanges() {}
 }
